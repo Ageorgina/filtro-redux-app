037b2c406749b2b0af843b20e261821e0e00cf4b
Ana Ortega <aortega@asgbpm.com>
2019-07-31 11:25:00 -0500

    Primer comit

---
+++ src/app/app.reducers.ts
@@ -0,0 +1,19 @@
+import { Todo } from './todo/model/todo.model';
+import { ActionReducerMap } from '@ngrx/store';
+import * as fromTodo from './todo/todo.reducer';
+import * as fromFiltro from './filter/filter.reducer';
+import * as fromFiltroActions from './filter/filter.actions';
+import { todoReducer } from './todo/todo.reducer';
+
+export interface AppState {
+    todos: Todo[];
+    //filtro: string; -->CORRECTO PERO HAY OTRA OPCION
+    filtro: fromFiltroActions.filtrosValidos;
+
+}
+
+export const appReducers: ActionReducerMap<AppState> = {
+    todos: fromTodo.todoReducer,
+    filtro: fromFiltro.filtroReducer,
+
+};
